cb5f1125781af7b93d54e2b8f01b669d4594b6ce
IoriKaede <paranoia170214@gmail.com>
2026-01-14 10:11:33 +0100

    1

--- simplex.py
+++ simplex.py
@@ -18,9 +18,10 @@
   "basis": [ 3, 4, 5 ]
 }'''
 
-
-import numpy as np
 from lp import LP
+import numpy as np
+
+
 def PrimalSimplex(A, b, c, B):
   # Set N:= {1,2, ...,k} \ B
   n = len(A[0])
@@ -32,6 +33,7 @@ def PrimalSimplex(A, b, c, B):
       pass
   count = 0
   while count < 9999:
+    print(count)
     #Solve A_{*,B} x_B = b for x_B ∈ R^B and set x_N := O_N
     #Solve y⊺A_{*,B} = c_B for y ∈ R^m and compute ¯c_j := c_j − y⊺A_{*,B} for all j ∈ N
     A_B = A[:, B]
@@ -48,7 +50,7 @@ def PrimalSimplex(A, b, c, B):
     #If ¯c_N ≥ O then return (“optimal”, solution x, optimal basis B)
     #if np.all(c_bar > -1e-7):
     if np.all(c_bar[N] > -1e-7):
-        return {"status": "optimal", "primal": x, "dual" : y}
+        return {"status": "optimal", "primal": x, "dual" : y, "basis":B}  #add basis as return
                     # small change: c_bar[N] instead of c_bar
                     # In c_bar we also have c_bar[B] which should be zero's, but due to noise or if indexing is mixed up, it can get <0
 
@@ -64,7 +66,7 @@ def PrimalSimplex(A, b, c, B):
 
     #If d ≥ O_N then return (“unbounded“, solution x, unbounded direction d).
     if np.all(d > -1e-7):
-      return {"status": "unbounded", "primal": x, "dual" : y, "ray":d} #direction included
+      return {"status": "unbounded", "primal": x, "dual" : y, "ray":d, "basis":B} #direction included
 
     #Compute θ* := min{−x_j/d_j | j ∈ B, d_j < 0}
     ratio = []  # ratio test
@@ -86,7 +88,35 @@ def PrimalSimplex(A, b, c, B):
     N.append(B_l)
                     # small change: Changed N[N.index(k)], since index doesn't matter. Have to make sure it actually removes k.
     count += 1
-  return {"status" : "limit reached", "primal" : x , "dual" : y}
+  return {"status" : "limit reached", "primal" : x , "dual" : y, "basis":B}
+
+def PrimalSimplex_without_basis(A,b,c):
+  m = lp.num_rows
+  n = lp.num_columns
+  A_bar = np.hstack((A , np.identity(m)))
+  b_bar = b
+  c_bar = np.zeros(n+m)
+  c_bar[n:] = 1
+  B_bar = []
+  for i in range(n,n+m):
+    B_bar.append(i)
+  print("phase 1")
+  dic = PrimalSimplex(A_bar, b_bar, c_bar, B_bar)
+  print("after phase 1")
+  status = dic["status"]
+  x_prime = dic["primal"]
+  B_prime = dic["basis"]
+  B_p = list(B_prime)
+  print(x_prime)
+
+  if status =="limit reached":
+    return {"status":"limit reached"}
+  if c_bar.T @ x_prime > 0 :
+    return {"status":"infeasible"}
+
+
+
+
 
 
 def solve(lp):
@@ -101,13 +131,22 @@ def solve(lp):
     b_list.append(float(con['rhs']))
   b = np.array(b_list)
   c = np.array(lp.objective)
+  for i in range(len(b)):
+    if any(bi <0 for bi in b):
+      A[i] = - A[i]
+      b[i] = - b[i]
             # we use arrays so we can use matrix multiplication
   # basis = lp.basis
             #small change: might not work if there is no basis given
   try:
     basis = lp.basis
   except KeyError:
-    basis = list(range(n-m, n))
+    basis = None
+  if basis is None:
+    basis = PrimalSimplex_without_basis(A, b, c)
+    if basis is None:
+      return {"status": "infeasible"}
+
             #small change: Use slack variables if no basis provided
 
 
@@ -120,7 +159,7 @@ def solve(lp):
 
 if __name__ == '__main__':
 
-  file_name = 'unbd-basis-agg3.json'
+  file_name = 'orig-std-adlittle.json'
   with open(file_name, 'r') as fp:
     lp = LP(fp.read())
     sol = solve(lp)
